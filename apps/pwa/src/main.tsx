a22372a4eb8b1fa0aaba36922de41b27da8b350c
YoelDevSoft1 <yoeldevsoft@gmail.com>
2025-12-31 15:44:51 -0500

    Update main.tsx

--- apps/pwa/src/main.tsx
+++ apps/pwa/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './styles/mobile-optimizations.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'react-hot-toast'
 
