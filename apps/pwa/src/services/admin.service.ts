55abd9c2d11ef7e479efb6c10097b0250b73c7c4
yoeldevsoft25 <yoeldevsoft@gmail.com>
2026-02-01 22:49:58 -0500

    a

--- apps/pwa/src/services/admin.service.ts
+++ apps/pwa/src/services/admin.service.ts
@@ -11,8 +11,9 @@ adminApi.interceptors.request.use(async (config) => {
   const baseUrl = getApiBaseUrl()
   config.baseURL = baseUrl
   if (baseUrl.includes('ngrok-free.dev')) {
-    config.headers = config.headers ?? {}
-    config.headers['ngrok-skip-browser-warning'] = '1'
+    if (config.headers) {
+      config.headers['ngrok-skip-browser-warning'] = '1'
+    }
   }
   return config
 })
--- apps/pwa/src/services/public-kitchen.service.ts
+++ apps/pwa/src/services/public-kitchen.service.ts
@@ -14,8 +14,9 @@ publicApi.interceptors.request.use(async (config) => {
   const baseUrl = getApiBaseUrl()
   config.baseURL = baseUrl
   if (baseUrl.includes('ngrok-free.dev')) {
-    config.headers = config.headers ?? {}
-    config.headers['ngrok-skip-browser-warning'] = '1'
+    if (config.headers) {
+      config.headers['ngrok-skip-browser-warning'] = '1'
+    }
   }
   return config
 })
