2cb03ef922a8a14c89c84e99f248d1b77eb7caaa
yoeldevsoft25 <trabajogalan2025@gmail.com>
2026-02-08 07:24:06 -0400

    Update skeleton.tsx

--- packages/ui-core/src/components/ui/skeleton.tsx
+++ packages/ui-core/src/components/ui/skeleton.tsx
@@ -1,12 +1,21 @@
+import { motion, HTMLMotionProps } from "framer-motion"
 import { cn } from "../../lib/utils"
 
 function Skeleton({
     className,
     ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: HTMLMotionProps<"div">) {
     return (
-        <div
-            className={cn("animate-pulse rounded-md bg-muted", className)}
+        <motion.div
+            animate={{
+                opacity: [0.5, 1, 0.5],
+            }}
+            transition={{
+                duration: 2,
+                repeat: Infinity,
+                ease: "easeInOut",
+            }}
+            className={cn("rounded-md bg-muted", className)}
             {...props}
         />
     )
